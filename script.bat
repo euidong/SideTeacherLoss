cd7ad08b297678815cc968df2c2965ba6aa523a1
euidong <justicedong@naver.com>
2023-05-21 16:19:14 +0900

    add window script

---
+++ script.bat
@@ -0,0 +1,11 @@
+python main.py --dataset="mnist" --alpha=0.005
+python main.py --dataset="mnist" --alpha=0.01
+
+python main.py --dataset="fashion_mnist" --alpha=0.005
+python main.py --dataset="fashion_mnist" --alpha=0.01
+
+python main.py --dataset="cifar10" --alpha=0.005
+python main.py --dataset="cifar10" --alpha=0.01
+
+python main.py --dataset="cifar100" --alpha=0.005
+python main.py --dataset="cifar100" --alpha=0.01
